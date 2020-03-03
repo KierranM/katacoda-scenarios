b6a7a44ae3752901b5368721412cfcb2cd89419e
Kierran McPherson <kierran.mcpherson@xero.com>
2020-03-04 07:53:03 +1300

    Wait for minikube to start

--- k8s-troubleshooting-01/init.sh
+++ k8s-troubleshooting-01/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 
+echo "Starting Kubernetes..."
+while [ `docker ps | wc -l` -eq 1 ]
+do
+  sleep 1
+done
+
+echo "Kubernetes Started"
+
 HOST_IP="$(ip -4 addr show ens3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 
 sed -i "s/HOSTIP/${HOST_IP}/g" ~/deployment.yaml
