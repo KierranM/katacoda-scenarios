173e6c1b36b74917179a9dac4adc565b6d7e4f93
Kierran McPherson <kierran.mcpherson@xero.com>
2020-03-04 09:01:57 +1300

    Fix the deployment apply

--- k8s-troubleshooting-01/init.sh
+++ k8s-troubleshooting-01/init.sh
@@ -13,4 +13,4 @@ HOST_IP="$(ip -4 addr show ens3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 sed -i "s/HOSTIP/${HOST_IP}/g" ~/my-website.yaml
 
 kubectl apply -f /opt/namespace.yaml
-kubectl apply ~/my-website.yaml -n my-website
+kubectl apply -f ~/my-website.yaml -n my-website
