e8e8237b3fbfa99b16049b30df9df690dfe7190a
Kierran McPherson <kierran.mcpherson@xero.com>
2020-03-04 09:10:55 +1300

    Wait 10s before creating deployment for RQ to load

--- k8s-troubleshooting-01/init.sh
+++ k8s-troubleshooting-01/init.sh
@@ -13,4 +13,7 @@ HOST_IP="$(ip -4 addr show ens3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 sed -i "s/HOSTIP/${HOST_IP}/g" ~/my-website.yaml
 
 kubectl apply -f /opt/namespace.yaml
+# Sleep to give the quote enough time to load
+sleep 10
+
 kubectl apply -f ~/my-website.yaml -n my-website
